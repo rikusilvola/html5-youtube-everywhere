49eceaa751f6baa744abf82f2069069e180585a2
riqpe <rsilvola@gmail.com>
2015-02-18 18:02:02 +0100

    added support for embedded legacy playlists and fixed a bug where every other embed on the page would not get replaced due to live HTMLCollection

--- data/flash2html.js
+++ data/flash2html.js
@@ -1,24 +1,64 @@
-Array.prototype.forEach.call(
-	document.getElementsByTagName("iframe"),
-	function (iframe) {
-		if (!iframe.src.match(/youtube\.(googleapis\.)?com/) || iframe.src.match(/html5=1/)) {
-			return;
+(function () {
+	this.getSrcParams = function( src ) {	
+		var query = src.replace(/^[^\?]+\??/,'');
+		var Params = new Object ();
+		if ( ! query ) return Params; // return empty object
+		var Pairs = query.split(/[;&]/);
+		for ( var i = 0; i < Pairs.length; i++ ) {
+			var KeyVal = Pairs[i].split('=');
+			if ( ! KeyVal || KeyVal.length != 2 ) continue;
+			var key = unescape( KeyVal[0] );
+			var val = unescape( KeyVal[1] );
+			val = val.replace(/\+/g, ' ');
+			Params[key] = val;
+			console.log(key + " = " + val);
 		}
-		iframe.src += (-1 === iframe.src.indexOf("?")) ? "?html5=1" : "&html5=1";
-});
-Array.prototype.forEach.call(
-	document.getElementsByTagName("embed"),
-	function (embed) {
-		if (!embed.src.match(/youtube\.(googleapis\.)?com/)) {
-			return;
+		return Params;
+	}
+	var iframes = document.getElementsByTagName("iframe");
+	for (var iframe of iframes) {
+		if (iframe.src.match(/youtube\.(googleapis\.)?com/) && !iframe.src.match(/html5=1/)) {
+			iframe.src += (-1 === iframe.src.indexOf("?")) ? "?html5=1" : "&html5=1";
 		}
-		var videoID = embed.src.match(/\/v\/([^&]+)/)[1];
-		var iframe = document.createElement("iframe");
-		iframe.width = embed.width;
-		iframe.height = embed.height;
-		iframe.type = "text/html";
-		iframe.frameBorder = "0";
-		iframe.src = "//www.youtube.com/embed/" + videoID + "?html5=1";
-		embed.parentNode.insertBefore(iframe, embed);
-		embed.parentNode.removeChild(embed);
-});
+	}
+	var embeds = []; // create a static copy of selected DOM elements
+	for (var embed of document.getElementsByTagName("embed")) {
+		embeds.push(embed);
+	}
+	for (var embed of embeds) {
+		if (embed.src.match(/youtube\.(googleapis\.)?com/)) {
+			var video = embed.src.match(/\/v\/([^&]+)/);
+			var playlist = embed.src.match(/\/p\/([^&]+)/);
+			if (video || playlist) {
+				var iframe = document.createElement("iframe");
+				if (video && video[1]) {	// video ID is the first match within match
+					iframe.src = "//www.youtube.com/embed/" + video[1] + "?html5=1";
+					var params = this.getSrcParams(embed.src);
+					if (params.list) {
+						iframe.src += "&list=" + params.list;
+					}
+				}
+				else if (playlist && playlist[1]) { // playlist ID is the first match within match
+					iframe.src = "//www.youtube.com/embed/videoseries?list=" + playlist[1] + "&html5=1";
+					var params = this.getSrcParams(embed.src);
+					if (params.v) {
+						iframe.src += "&v=" + params.v;
+					}
+				}
+				else { 
+					console.log("No matching video / playlist params");
+					continue;
+				}
+				iframe.width = embed.width;
+				iframe.height = embed.height;
+				iframe.type = "text/html";
+				iframe.frameBorder = "0";
+				embed.parentNode.insertBefore(iframe, embed);
+				embed.parentNode.removeChild(embed);
+			}
+			else {
+				console.log("No video or playlist in embed src");
+			}
+		}
+	}
+})();
